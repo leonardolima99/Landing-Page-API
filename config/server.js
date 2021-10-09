f6daf9a84612ade8fed7c0f8a541700b6f7c3804
Leonardo Lima <leonardo1230987@gmail.com>
2021-10-09 12:45:21 -0400

    Estudando o Strapi.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '701a02dce483c83444a180726f124b7d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1b49019c-8623-4f5f-b691-8abaf230dfde'
+};
